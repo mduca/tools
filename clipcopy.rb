a6df9b166a22f8f2d87ad93b1e16b397e03c8961
Michael Manduca <michael@mandu.ca>
2015-09-17 14:12:09 -0700

    Update comments for usage

--- clipcopy.rb
+++ clipcopy.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
-# ctrl-c long url to clipboard
-# run this script
-# ctrl-v to paste short goo.gl url
+# Copy any long form url to your clipboard. (ctrl+c)
+# Then run this script. After the script executes
+# the new shortened goo.gl url will be in the
+# clibpboard ready to be pasted. (ctrl+v)
 
 require "clipboard"
 require "httparty"
@@ -12,11 +13,15 @@ class Google
   format :json
 end
 
+# Example url-config.json
+# {
+#   "url" : "https://www.googleapis.com/urlshortener/v1/url",
+#   "api" : "AIzaSyBW5fsScqPZsv2OjUhgqnrIjfZFhwlGFTw"
+# }
 
 fname = File.join(File.dirname(__FILE__), "confs/url-config.json")
 file = File.read(fname)
 config = JSON.parse(file)
-
 
 clip = Clipboard.paste
 
@@ -26,4 +31,5 @@ response = Google.post(config["url"],
   :body =>    { "longUrl" => clip }.to_json
 )
 
+# Copy to clipboard
 Clipboard.copy response["id"].to_s
