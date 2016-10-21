1666b595e8f486149a24dd677b5685c258c59383
mduca <michael@mandu.ca>
2016-10-20 20:33:58 -0700

    detect Vagrantfiles

--- actuate.sh
+++ actuate.sh
@@ -7,31 +7,43 @@
 # install pathogen
 # setup vim plugins
 
+
+user=$USER
+
+provision="/vagagrant/Vagrantfile"
+if [ -e $provision ]
+then
+  echo "Vagrantfile found...."
+  echo "Setting \$USER to vagrant...."
+  $user=vagrant
+fi
+
+
 if hash git 2>/dev/null; then
   echo "Cloning repos and plugins...."
-  git clone http://github.com/mduca/.dotfiles.git /home/$USER/.dotfiles
+  git clone http://github.com/mduca/.dotfiles.git /home/$user/.dotfiles
 
-  ln -s /home/$USER/.dotfiles/vimrc /home/$USER/.vimrc
-  ln -s /home/$USER/.dotfiles/bashrc /home/$USER/.bashrc
-  ln -s /home/$USER/.dotfiles/tmux.conf /home/$USER/.tmux.conf
-  ln -s /home/$USER/.dotfiles/dircolors /home/$USER/.dircolors
-  ln -s /home/$USER/.dotfiles/irbrc /home/$USER/.irbrc 
-  ln -s /home/$USER/.dotfiles/gemrc /home/$USER/.gemrc 
+  ln -s /home/$user/.dotfiles/vimrc /home/$user/.vimrc
+  ln -s /home/$user/.dotfiles/bashrc /home/$user/.bashrc
+  ln -s /home/$user/.dotfiles/tmux.conf /home/$user/.tmux.conf
+  ln -s /home/$user/.dotfiles/dircolors /home/$user/.dircolors
+  ln -s /home/$user/.dotfiles/irbrc /home/$user/.irbrc 
+  ln -s /home/$user/.dotfiles/gemrc /home/$user/.gemrc 
 
-  mkdir /home/$USER/.vim
-  mkdir -p /home/$USER/.vim/autoload /home$USER/.vim/bundle
+  mkdir /home/$user/.vim
+  mkdir -p /home/$user/.vim/autoload /home$user/.vim/bundle
 
-  curl -LSso /home/$USER/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  curl -LSso /home/$user/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-  git clone https://github.com/scrooloose/nerdtree.git /home/$USER/.vim/bundle/nerdtree
-  git clone https://github.com/tpope/vim-surround.git /home/$USER/.vim/bundle/surround
-  git clone https://github.com/tpope/vim-rails.git /home/$USER/.vim/bundle/rails
-  git clone https://github.com/Lokaltog/vim-easymotion /home/$USER/.vim/bundle/vim-easymotion
-  git clone https://github.com/ervandew/supertab.git /home/$USER/.vim/bundle/supertab
-  git clone https://github.com/scrooloose/syntastic.git /home/$USER/.vim/bundle/syntastic
+  git clone https://github.com/scrooloose/nerdtree.git /home/$user/.vim/bundle/nerdtree
+  git clone https://github.com/tpope/vim-surround.git /home/$user/.vim/bundle/surround
+  git clone https://github.com/tpope/vim-rails.git /home/$user/.vim/bundle/rails
+  git clone https://github.com/Lokaltog/vim-easymotion /home/$user/.vim/bundle/vim-easymotion
+  git clone https://github.com/ervandew/supertab.git /home/$user/.vim/bundle/supertab
+  git clone https://github.com/scrooloose/syntastic.git /home/$user/.vim/bundle/syntastic
 
   clear
-  source /home/$USER/.bashrc
+  source /home/$user/.bashrc
   echo "Actuated..."
 
 else
